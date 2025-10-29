aeddc32228c918c05b1794acef8d9f1f840cc002
Gabriel Matias <ggezcom991gh@gmail.com>
2025-10-29 19:16:03 -0300

    Corrigindo caminho pro supaBaseClient dentro de addUser.

--- api/addUser.js
+++ api/addUser.js
@@ -1,4 +1,4 @@
-import { supabase } from '../src/supaBaseClient.js'
+import { supabase } from "./src/supaBaseClient.js"
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
