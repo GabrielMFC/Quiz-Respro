c982e5eb54271d12d61eaad0d7935f38648e368a
Gabriel Matias <ggezcom991gh@gmail.com>
2025-10-29 18:58:50 -0300

    Corrigindo o nome das chaves na criação do client do SupaBase.

--- api/src/supaBaseClient.js
+++ api/src/supaBaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
+const SUPABASE_URL = process.env.SUPABASE_PROJECT_URL
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_KEY
 
 if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error('Variáveis de ambiente do Supabase não foram definidas.')
